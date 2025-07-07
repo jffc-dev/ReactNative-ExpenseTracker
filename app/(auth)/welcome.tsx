cd5bb50db094ec3150ae4c923a9567ef4853a703
Javier Flores <jffc.dev@gmail.com>
2025-07-06 22:05:39 -0500

    feat: adding initial components

---
+++ app/(auth)/welcome.tsx
@@ -0,0 +1,15 @@
+import ScreenWrapper from '@/components/ScreenWrapper'
+import React from 'react'
+import { StyleSheet, Text } from 'react-native'
+
+const Welcome = () => {
+  return (
+    <ScreenWrapper>
+      <Text style={{color: 'white'}}>Welcome</Text>
+    </ScreenWrapper>
+  )
+}
+
+export default Welcome
+
+const styles = StyleSheet.create({})
